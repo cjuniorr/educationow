fbc558fdc36ccebdeb4cb753ee6f22fd42dbe115
Célio Teixeira Paiva Junior <celiopaivajr@gmail.com>
2020-04-05 15:39:54 -0300

    Ajusta objeto aluno

--- EducatioNow/EducatioNow/Models/Aluno.cs
+++ EducatioNow/EducatioNow/Models/Aluno.cs
@@ -4,9 +4,7 @@ namespace EducatioNow.Models
 {
     public class Aluno
     {
-        private Aluno() { }
-
-        public int Id { get; set; }
+        public int? Id { get; set; }
 
         public string TurmaId { get; set; }
 
